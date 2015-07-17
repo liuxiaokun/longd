c0368a203851182dfcc5bb4311bc626d6883c5fc
Fred Liu <liuxiaokun0410@qq.com>
2015-07-17 18:32:53 +0800

    add application

---
+++ app/src/main/java/com/fred/longd/application/MyApplication.java
@@ -0,0 +1,12 @@
+package com.fred.longd.application;
+
+import android.app.Application;
+
+/**
+ * @author Fred Liu(liuxiaokun0410@gmail.com)
+ * @version 1.0
+ * @date 2015/7/17
+ */
+public class MyApplication extends Application {
+
+}
